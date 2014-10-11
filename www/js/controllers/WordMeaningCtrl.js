283b76a1cb7ddb59a24ed806a2c6f94122d79def
Nadim Tuhin <nadimtuhin@gmail.com>
2014-10-12 04:55:59 +0600

    lots of

--- www/js/controllers/WordMeaningCtrl.js
+++ www/js/controllers/WordMeaningCtrl.js
@@ -2,4 +2,5 @@ angular.module('ortho')
 .controller('WordMeaningCtrl', ["$scope", "$stateParams", "Words",
     function($scope, $stateParams, Words) {
         $scope.word = Words.get($stateParams.wordId);
+        $scope.word.mn = _.uniq($scope.word.mn);
 }]);
