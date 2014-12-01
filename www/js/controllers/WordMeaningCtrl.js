8fcd0a7b5c7416502162dd9694dab04b415d4bc9
Nadim Tuhin <nadimtuhin@gmail.com>
2014-12-02 00:51:38 +0600

    removed back button from meaning view

--- www/js/controllers/WordMeaningCtrl.js
+++ www/js/controllers/WordMeaningCtrl.js
@@ -1,10 +1,10 @@
 angular.module('ortho')
-.controller('WordMeaningCtrl', ["$scope", "$stateParams", "Words", "$ionicNavBarDelegate",
-    function($scope, $stateParams, Words, $ionicNavBarDelegate) {
+.controller('WordMeaningCtrl', ["$scope", "$stateParams", "Words", "$timeout", "$ionicNavBarDelegate",
+    function($scope, $stateParams, Words, $timeout, $ionicNavBarDelegate) {
 		$timeout( function() {
 			$ionicNavBarDelegate.showBackButton(false);
 		}, 0);
-		
+
         $scope.word = Words.get($stateParams.wordId);
         $scope.word.mn = _.uniq($scope.word.mn);
 }]);
--- www/js/script.js
+++ www/js/script.js
@@ -240,12 +240,12 @@ angular.module('ortho')
         $scope.words = Words.paginate($scope.page, $scope.perPage, topWords);
 }]);
 angular.module('ortho')
-.controller('WordMeaningCtrl', ["$scope", "$stateParams", "Words", "$ionicNavBarDelegate",
-    function($scope, $stateParams, Words, $ionicNavBarDelegate) {
+.controller('WordMeaningCtrl', ["$scope", "$stateParams", "Words", "$timeout", "$ionicNavBarDelegate",
+    function($scope, $stateParams, Words, $timeout, $ionicNavBarDelegate) {
 		$timeout( function() {
 			$ionicNavBarDelegate.showBackButton(false);
 		}, 0);
-		
+
         $scope.word = Words.get($stateParams.wordId);
         $scope.word.mn = _.uniq($scope.word.mn);
 }]);
