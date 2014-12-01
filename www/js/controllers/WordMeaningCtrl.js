346351f820845e26076722dd71b6f26349ec2954
Nadim Tuhin <nadimtuhin@gmail.com>
2014-12-02 00:48:56 +0600

    removed back button from meaning view

--- www/js/controllers/WordMeaningCtrl.js
+++ www/js/controllers/WordMeaningCtrl.js
@@ -1,6 +1,10 @@
 angular.module('ortho')
-.controller('WordMeaningCtrl', ["$scope", "$stateParams", "Words",
-    function($scope, $stateParams, Words) {
+.controller('WordMeaningCtrl', ["$scope", "$stateParams", "Words", "$ionicNavBarDelegate",
+    function($scope, $stateParams, Words, $ionicNavBarDelegate) {
+		$timeout( function() {
+			$ionicNavBarDelegate.showBackButton(false);
+		}, 0);
+		
         $scope.word = Words.get($stateParams.wordId);
         $scope.word.mn = _.uniq($scope.word.mn);
 }]);
--- www/js/script.js
+++ www/js/script.js
@@ -240,8 +240,12 @@ angular.module('ortho')
         $scope.words = Words.paginate($scope.page, $scope.perPage, topWords);
 }]);
 angular.module('ortho')
-.controller('WordMeaningCtrl', ["$scope", "$stateParams", "Words",
-    function($scope, $stateParams, Words) {
+.controller('WordMeaningCtrl', ["$scope", "$stateParams", "Words", "$ionicNavBarDelegate",
+    function($scope, $stateParams, Words, $ionicNavBarDelegate) {
+		$timeout( function() {
+			$ionicNavBarDelegate.showBackButton(false);
+		}, 0);
+		
         $scope.word = Words.get($stateParams.wordId);
         $scope.word.mn = _.uniq($scope.word.mn);
 }]);
